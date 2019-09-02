22d54c71d193a8309f1539b051f4d4514ff4fccd
leeks123 <kensei719@naver.com>
2019-09-02 23:57:50 +0900

    수정

--- automata_Python150/2822_점수계산.py
+++ automata_Python150/2822_점수계산.py
@@ -3,11 +3,7 @@
 dic = {}
 sum = 0
 for i in range(8):
-    n = int(input())
-    sum += n
-    dic[i+1] = n
-
-print(sum)
+    dic[i+1] = int(input())
 
 li = [] #파이썬3에서 dic_items()는 리스트를 반환하지 않음
 for k,v in dic.items():
@@ -16,5 +12,8 @@ for k,v in dic.items():
 li.sort(key=lambda x:x[1],reverse=True)
 li = li[0:5]
 li.sort(key=lambda x:x[0])
+for i in li:
+    sum+=i[1]
+print(sum)
 for i in li:
     print(i[0],end=' ')
